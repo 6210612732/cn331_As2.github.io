4a69dba4264670122ed79c670e3f8d63b8d8a711
pornchai phumphinyo <6210612732@student.tu.ac.th>
2021-09-22 15:34:15 +0000

    add heroku 7

--- regis/views.py
+++ regis/views.py
@@ -12,29 +12,31 @@ def index(request):
     #data_student = Student.objects.get(pk=2)
     #non_course = Course.objects.exclude(enroll=data_student).all  #show all of courses that member doesnt enroll
     #return render(request, "regis/mysub.html" , {'courses':non_course})
+    a1=''
+    a2=0
     a1=request.POST.get('a1', '');
     a2=request.POST.get('a2', 0);
 
     year = 0
     temp = User.objects.all().filter(id=request.user.id)
-    temp2 = temp.values_list('email') 
+    temp2 = temp.values_list('email')
     if temp2:
-        year = temp2[0][0] 
+        year = temp2[0][0]
         data = Course.objects.exclude(student=request.user.id).filter(for_year=year)
     else:
         data = Course.objects.exclude(student=request.user.id) #.filter(year=year[0][0]) # เอาช่อง isstaffเก็บชั้นปีละกัน
-    data2 = Course.objects.all() 
+    data2 = Course.objects.all()
     if a2!= '0' and a1 != '':
        data2 = Course.objects.all().filter(c_code=a1,for_year=a2)
     elif a2!='0':
        data2 = Course.objects.all().filter(for_year=a2)
     elif a1 != '':
        data2 = Course.objects.all().filter(c_code=a1)
-    
-        
-    return render(request, "regis/index.html",{'courses':data,"data2":data2,"year":year,"a1":a1,"a2":a2}) 
-   
-    
+
+
+    return render(request, "regis/index.html",{'courses':data,"data2":data2,"year":year,"a1":a1,"a2":a2})
+
+
 def mysub(request):
     data = Course.objects.filter(student=request.user.id)
     return render(request, "regis/mysub.html",{'courses':data})
@@ -68,16 +70,16 @@ def course(request,course_id):
     check = Course.objects.get(pk=course_id)
     if request.user in check.student.all():
         status = 1
-    return render(request, "regis/course.html",{"course":course,"status":status}) 
+    return render(request, "regis/course.html",{"course":course,"status":status})
 
 #return render(request, "regis/mysub.html" , {'courses':non_course})
 #return render(request, "regis/mysub.html" , {'courses':data_course})
 
 # เหมือนว่ากล่องจะpaddingมากไป
-    
 
 
-    
-    
+
+
+
 
 
