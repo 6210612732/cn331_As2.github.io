37d87c8a9b50bec149668413d6407d66f168b67f
pornchai phumphinyo <6210612732@student.tu.ac.th>
2021-09-26 12:28:22 +0000

    add heroku 1

--- basic/settings.py
+++ basic/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'regis',
     'users',
 
-    
+
 ]
 
 MIDDLEWARE = [
@@ -125,7 +125,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'regis/static')]
-
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
