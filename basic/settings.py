45257e444f39ffac08b015707b381c6ad2acee8b
pornchai phumphinyo <6210612732@student.tu.ac.th>
2021-09-26 12:34:54 +0000

    add heroku 3

--- basic/settings.py
+++ basic/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-m$$pswz@zz8t+gb+rv$kkbihoqs4p=53t=bg^xw4xruq8y!8y*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -125,8 +125,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'regis/static')]
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+#STATIC_ROOT = os.path.join(BASE_DIR,'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+import django_heroku
+django_heroku.settings(locals())
