bfb5ed6d4d49a9d67f8ac4445821eb44be330e71
pornchai phumphinyo <6210612732@student.tu.ac.th>
2021-09-26 12:30:34 +0000

    add heroku 2

--- basic/settings.py
+++ basic/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-m$$pswz@zz8t+gb+rv$kkbihoqs4p=53t=bg^xw4xruq8y!8y*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
